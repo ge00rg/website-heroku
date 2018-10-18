0d80162a91e16671bdf570bd4c2fcd224a5951b5
JoramKeijser <JoramKeijser@users.noreply.github.com>
2018-10-18 16:50:03 +0200

    Create 2019-1-16-Macke-Jakob.m

---
+++ _talks/2019-1-16-Macke-Jakob.m
@@ -0,0 +1,11 @@
+---
+layout: post_event
+title: Invited talk seminar series ' statistical analysis of neural data' 
+date: January 16, 2019, 10:15
+speaker: Jakob Macke
+location: Bernstein Center, main lecture hall 
+affiliation: TU München)
+---
+
+[Bernstein Center, Philippstr. 13, Haus 6, 10115 Berlin](https://www.bccn-berlin.de/how-to-reach-bccn.html)
+
